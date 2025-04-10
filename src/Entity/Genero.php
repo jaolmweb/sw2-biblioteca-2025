79180550e059c8b76636262417fc4b7e70ea1e15
jaolmweb <lkira1314@gmail.com>
2025-04-10 01:19:50 +0000

    Entidade Genero

---
+++ src/Entity/Genero.php
@@ -0,0 +1,26 @@
+<?php
+namespace Application\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity, Table(name : 'generos')]
+class Genero {
+    #[Id, Column(type: 'integer'), GeneratedValue(strategy: 'AUTO')]
+    private int $id;
+    #[Column(type: 'string', nullable: false)]
+    private string $nome;
+
+    public function getId() : int {
+        return $this->id;
+    }
+    public function setId(int $d) : string {
+        return $this->nome;
+    }   
+    public function setNome(string $nome) : void {
+        $this->nome = $nome; 
+    }
+}
